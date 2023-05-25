420f1dff56afd1eb8d44e4e1f9323f915212c6f2
anh3214 <ptuananh170@gmail.com>
2023-05-26 04:24:28 +0700

    Nearly Tower

---
+++ Assets/Scripts/Towers/MagicTower/MagicTower.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicTower : Tower,ITower
+{
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void BuildTower()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void FindNearestEnemy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SellTower()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateTower()
+    {
+        throw new System.NotImplementedException();
+    }
+}
